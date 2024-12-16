bdbaa13d5f2f2f82446060bf16114efc509ad092
alcides1819 <78455793+alcides1819@users.noreply.github.com>
2024-12-16 10:57:13 -0500

    script

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function mostrar(){
+function bachiller(){
     location.href="bachiller.jpg"
+}
+function sistemas(){
+    location.href="sistemas.jpg"
+}
+function mineria(){
+    location.href="mineria.jpg"
 }
